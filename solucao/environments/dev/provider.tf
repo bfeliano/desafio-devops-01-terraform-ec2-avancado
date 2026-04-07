27bf7239644de65ec8100a71c12d44f9bd4cad61
bfeliano <bfeliano@gmail.com>
2026-04-07 11:15:45 +0200

    adicionar arquivos de configuracao + dev environment

---
+++ solucao/environments/dev/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.37.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "axionis-bucket-tf-state"
+    key    = "desafio-01/dev/terraform.tfstate"
+    region = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
